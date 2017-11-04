f7d8cecaa9a364b001bd9ef0847058252e7f14af
Maria Burlando <maria@dmm888.com>
2017-11-04 17:15:58 +0100

    Implemented ajax function in button click to update flower notes in the textarea

--- scripts/adminflowers.js
+++ scripts/adminflowers.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
    $.ajax({
    		url: "http://localhost:3000/flowers",
-   	 	beforeSend: function (xhr) {   
+   	 	beforeSend:  (xhr)=> {   
 		$('#loader').show();
    	 		
    	 	},
-	 	success: function(result){	
+	 	success: (result)=>{	
 
 	 	$('#loader').hide(); 		
 	 		for (let res of result) {
@@ -14,16 +14,26 @@ $(document).ready(() => {
 	 			let appropriateTag = res.Name.replace(/ /g, "_");
 	 			
 	 			$('#flower-section').append(`<div style="background-color:${res.FlowerColor.colorCode}" class="flex-container"><h1>${res.Name}</h1><textarea data-flower = "${appropriateTag}" placeholder="Notes to update">${res.Notes}</textarea><button id = "${appropriateTag}">Update</button>`);
-				$('#'+appropriateTag).click(function() {
-  				let myTextarea = $(`textarea[data-flower ="${appropriateTag}"]`);
-  				let flowerName = res.Name;
-  				
-
-
+				$('#'+appropriateTag).click(() => {
+  					let notesTextarea = $(`textarea[data-flower ="${appropriateTag}"]`);
+  					let flowerName = res.Name;
+  					// *** Ajax call invoked when the update button is clicked
+  					$.ajax({
+  						url: 'http://localhost:3000/updateflowernotes',
+  						method: 'POST',
+  						data: { Name:flowerName, Notes:notesTextarea.val() },
+  						success: (response) =>{
+  							alert(response);
+  						},
+  						error: (error)=> {
+   							alert(error);
+   						}
+  					});
+  					// *** End Ajax call
 				});
 	 		}    	
    		},
-   		error: function(error) {
+   		error: (error) =>{
    		alert(error);
    		}
 	});
